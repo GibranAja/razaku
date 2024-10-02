9466ddb63a23c2c552886e4d6a84c76c5bbd7045
GibranAja <mgibranarraffi@gmail.com>
2024-10-02 20:25:30 +0700

    feat: new route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LandingPage from '../views/LandingPage.vue'
+import CaseCategory from '../views/CaseCategory.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +10,12 @@ const router = createRouter({
       name: 'home',
       component: LandingPage
     },
-
+    {
+      path: '/case/:category',
+      name: 'caseCategory',
+      component: CaseCategory,
+      props: true
+    }
   ]
 })
 
