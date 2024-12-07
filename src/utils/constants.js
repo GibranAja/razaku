c0917a8adaa74d067b6c6cf6d2f9cd0e33c63b5c
GibranAja <mgibranarraffi@gmail.com>
2024-12-07 19:46:49 +0700

    feat: add kurirStandar

---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+export const rajaOngkir = {
+    baseUrl : 'https://api.rajaongkir.com/starter',
+    apiKey : 'f5bbc8efa8f9454734a47ea7558f1737'
+}
+
+export const kurirStandar = [
+    {id: 'jne', name: 'JNE'},
+    {id: 'pos', name: 'Pos Indonesia'},
+    {id: 'tiki', name: 'Tiki'}
+]
